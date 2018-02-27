a24b935a837aaae552dab33192846b69ca34b087
Rubens Della Matta <rubensdellamatta@hotmail.com>
2018-02-27 20:49:21 -0300

    novo2

---
+++ aula02/logicos.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<pre>";
+var_dump(true and true);
+var_dump(true and false);
+var_dump(true && true);
+var_dump(true && false);
+echo "<hr>";
+var_dump(true or true);
+var_dump(true or false);
+var_dump(true || true);
+var_dump(true || false);
+echo "<hr>";
+var_dump(true xor true);
+var_dump(true xor false);
+var_dump(true xor true);
+var_dump(true || false);
+echo "<hr>";
+var_dump(!true)
+var_dump(!false);
