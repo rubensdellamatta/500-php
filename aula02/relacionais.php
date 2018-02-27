c4cdb14a351990f174a57acaf6dab2acc7792e69
Rubens Della Matta <rubensdellamatta@hotmail.com>
2018-02-27 20:30:34 -0300

    novo

---
+++ aula02/relacionais.php
@@ -0,0 +1,43 @@
+<?php
+
+//igual
+
+echo "<pre>";
+var_dump(2 == 2);
+var_dump(2 == "2");
+var_dump(2 == 3);
+echo "<hr>";
+
+// identico
+var_dump(2 === 2);
+var_dump(2 === "2");
+echo "<hr>";
+
+// diferente
+var_dump(2 != 2);
+var_dump(3 != "2");
+echo "<hr>";
+
+//menor que
+var_dump(2 > 5);
+var_dump(5 > 2);
+var_dump(5 > 5);
+echo "<hr>";
+
+//maior ou igual
+var_dump(2 >= 5);
+var_dump(5 >= 2);
+var_dump(2 >= 2);
+echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
+//maior ou igaul
