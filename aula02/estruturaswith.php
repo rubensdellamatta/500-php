f2963e3f06868faec94ea72495996683f4e9b84a
Rubens Della Matta <rubensdellamatta@hotmail.com>
2018-02-27 22:16:56 -0300

    novo4

---
+++ aula02/estruturaswith.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = readline_info("digite seu nome");
+
+switch ($nome) {
+	case 'rubens':
+		echo "eae rubens";
+		break;
+		case 'ronaldo':
+		echo "eae ronaldo";
+		break;
+	default:
+		echo "eae estranho"
+		break;
+}
